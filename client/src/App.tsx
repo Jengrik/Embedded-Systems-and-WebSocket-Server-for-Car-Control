ee5133174bb060abc40726430393b34134f64974
Jhon Navarrete <jhonerik15769@hotmail.com>
2024-03-31 00:40:58 -0500

    The client part was created and the CORS was solved using the Vite Config.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+//* Import Files and Packages
+//import io from "socket.io-client";
+import "./App.css";
+
+//const socket = io("/");
+
+function App() {
+  return <div>Hello from the Client!</div>;
+}
+
+export default App;
